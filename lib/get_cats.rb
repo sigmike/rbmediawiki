9bcf3d3fb3126dd019d5d4612a3b8d1bf6e603f5
Chabacano Chabacano <chabawiki@gmail.com>
2009-06-02 02:36:53 +0200

    test file rmed

--- lib/get_cats.rb
+++
@@ -1,7 +0,0 @@
-require './rbmediawiki'
-
-site = Api.new(nil, nil, 'chabacano', 'http://es.wikipedia.org', 'http://es.wikipedia.org/w/api.php')
-page = Page.new(ARGV[0],site)
-cl = page.get_categories()
-puts cl ? cl.size : 0
-
