aeef08e6a307367a107e2183a292245d7123db93
Piglop <mike@lepton.fr>
2009-10-10 11:49:47 +0200

    added failing test...

--- test/test_rbmediawiki.rb
+++ test/test_rbmediawiki.rb
@@ -1,8 +1,13 @@
+$: << File.join(File.dirname(__FILE__), "..", "lib")
+
 require "test/unit"
 require "rbmediawiki"
+require 'pp'
 
 class TestRbmediawiki < Test::Unit::TestCase
-  def test_sanity
-    flunk "write tests or I will kneecap you"
+  def test_retrieve_piglobot_contributions
+    site = Api.new(nil, nil, nil, 'http://fr.wikipedia.org', 'http://fr.wikipedia.org/w/api.php')
+    piglobot = User.new("Piglobot", site)
+    assert piglobot.get_usercontribs(5)
   end
 end
